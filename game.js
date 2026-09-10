33f8cab488b3fbf88514facfdb083a1db118fa22
adityagaonkar3456 <adityagaonkar3456@gmail.com>
2026-09-10 17:10:38 +0530

    Log message when THREE.js is loaded

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
-
+console.log("THREE.JS LOADED");
 /* =========================
    BASIC SETUP
 ========================= */
