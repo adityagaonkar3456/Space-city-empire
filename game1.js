0734ecca7984954789864c8273df3657e4103c2c
adityagaonkar3456 <adityagaonkar3456@gmail.com>
2026-09-11 13:36:29 +0530

    Add GLTFLoader import to game1.js

--- game1.js
+++ game1.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
-
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
 export function createGame1(canvas) {
 
     const scene = new THREE.Scene();
